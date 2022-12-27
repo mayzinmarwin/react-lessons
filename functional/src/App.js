89313fe0d09fdfcdb0ba4cb2a9ba3ab95b330cc0
mayzinmarwin <mayz97990@gmail.com>
2022-12-27 15:03:29 +0630

    react-todo-list

--- functional/src/App.js
+++ functional/src/App.js
@@ -1,12 +1,16 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from "./components/Header";
 import Form  from './components/Form';
 import TodoList from './components/TodoList';
 
 const App = () =>{
+    const initialState = JSON.parse(localStorage.getItem("todos")) || [];
     const [input,setInput] = useState("");
-    const [todos,setTodos] = useState([]);
+    const [todos,setTodos] = useState(initialState);
     const [editTodo,setEditTodo] =useState(null);
+    useEffect(()=>{
+        localStorage.setItem("todos",JSON.stringify(todos));
+    },[todos]);
     return (
         <div className="container">
             <div className="app-wrapper">
